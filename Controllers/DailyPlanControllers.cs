91c69019b2070efd4c7c23be2eb5279683e0a955
tab <140590830+7un9AnhI3ui@users.noreply.github.com>
2024-10-15 14:42:44 +0800

    Update DailyPlanControllers.cs

--- Controllers/DailyPlanControllers.cs
+++ Controllers/DailyPlanControllers.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using DailyPlanManager.Models;
 using DailyPlanManager.Data;
 
@@ -18,12 +18,15 @@ public class DailyPlanController : Controller
     {
         HttpContext.Session.SetInt32("user", id);
         var userplan = _context.DailyPlan.Where(plan => plan.User_Id == id).ToList();
-        if (userplan == null)
+
+        if (userplan == null || !userplan.Any()) // Check if list is empty
         {
             return NotFound();
         }
+
         return View(userplan);
     }
+
     public IActionResult Create()
     {
         var user = HttpContext.Session.GetInt32("user");
@@ -44,7 +47,66 @@ public class DailyPlanController : Controller
             _context.SaveChanges();
             return RedirectToAction(nameof(Index), new { id = user.Value });
         }
-        return View(dailyPlan);
+        else
+        {
+            return View(dailyPlan);
+        }
+    }
+}
+
+using Microsoft.AspNetCore.Mvc;
+using DailyPlanManager.Models;
+using DailyPlanManager.Data;
+
+namespace DailyPlanManager;
+
+public class DailyPlanController : Controller
+{
+
+    private readonly ApplicationDbContext _context;
+
+    
+    public DailyPlanController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public IActionResult Index(int id)
+    {
+        HttpContext.Session.SetInt32("user", id);
+        var userplan = _context.DailyPlan.Where(plan => plan.User_Id == id).ToList();
+
+        if (userplan == null || !userplan.Any()) // Check if list is empty
+        {
+            return NotFound();
+        }
+
+        return View(userplan);
+    }
+
+    public IActionResult Create()
+    {
+        var user = HttpContext.Session.GetInt32("user");
+        if (user == null)
+        {
+            return Redirect("https://localhost:7289");
+        }
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Create(DailyPlan dailyPlan)
+    {
+        var user = HttpContext.Session.GetInt32("user");
+        dailyPlan.User_Id = user;
+        if (ModelState.IsValid)
+        {
+            _context.DailyPlan.Add(dailyPlan);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index), new { id = user.Value });
+        }
+        else
+        {
+            return View(dailyPlan);
+        }
     }
 }
 
