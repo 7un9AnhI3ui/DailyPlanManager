f0c1ab891e15accb132ce4e5dd60bcddfecaffba
tab <140590830+7un9AnhI3ui@users.noreply.github.com>
2024-10-15 14:34:13 +0800

    Update UserController.cs

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using DailyPlanManager.Models;
 using DailyPlanManager.Data;
@@ -7,28 +7,35 @@ namespace DailyPlanManager;
 
 public class UserController : Controller
 {
-    //private readonly ILogger<UserController> _logger;
-    private readonly ApplicationDbContext _context;
 
-    /*public UserController(ILogger<UserController> logger)
-    {
-        _logger = logger;
-    }*/
+    private readonly ApplicationDbContext _context;
 
     public UserController(ApplicationDbContext context)
     {
         _context = context;
     }
+
     public IActionResult Index()
     {
+        List<User> users = new List<User>(); // Initialize an empty list by default
+
         if (_context.User != null)
         {
-            List<User> users = _context.User.ToList();
-            return View(users);
-        } 
-        var messages = "No user created";
-        return View(messages);
+            users = _context.User.ToList();
+        }
+
+        if (users.Any()) // Check if the list has any users
+        {
+            ViewBag.Message = "Select Users:";
+        }
+        else
+        {
+            ViewBag.Message = "No user created";
+        }
+
+        return View(users); // Always return a list (either empty or populated)
     }
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
